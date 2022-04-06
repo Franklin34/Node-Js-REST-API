c1a8d91880b51334d2b128d98ec3bd8f1ec34bd8
Franklin34 <franklin.sandoval42@gmail.com>
2022-04-05 22:57:51 -0600

    REST API Products

---
+++ src/routes/products.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router()
+
+import * as productsCtrl from '../controllers/products.controller'
+
+router.get('/', productsCtrl.getProducts)
+router.post('/', productsCtrl.createProduct)
+router.put('/:productId', productsCtrl.updateProductById)
+router.delete('/:productId', productsCtrl.deleteProductById)
+router.get('/:productId', productsCtrl.getProductById)
+
+
+export default router

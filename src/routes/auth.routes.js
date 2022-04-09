45c6bd3d7e1d3d3b142d5aa0969dfb280d0ebd5e
Franklin34 <franklin.sandoval42@gmail.com>
2022-04-08 23:59:49 -0600

    REST API with authentication and authorization almost done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,4 +2,9 @@ import { Router } from "express";
 
 const router = Router()
 
+import * as authCtrl from '../controllers/auth.controller'
+
+router.post('/signUp', authCtrl.signUp)
+router.post('/signIn', authCtrl.signIn)
+    
 export default router
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,12 +3,12 @@ import { Router } from "express";
 const router = Router()
 
 import * as productsCtrl from '../controllers/products.controller'
+import {authJwt} from '../middlewares'
 
 router.get('/', productsCtrl.getProducts)
-router.post('/', productsCtrl.createProduct)
-router.put('/:productId', productsCtrl.updateProductById)
-router.delete('/:productId', productsCtrl.deleteProductById)
+router.post('/', [authJwt.verifyToken, authJwt.isModerator], productsCtrl.createProduct)
+router.put('/:productId', [authJwt.verifyToken, authJwt.isModerator], productsCtrl.updateProductById)
+router.delete('/:productId',  [authJwt.verifyToken, authJwt.isModerator], productsCtrl.deleteProductById)
 router.get('/:productId', productsCtrl.getProductById)
-
 
 export default router
